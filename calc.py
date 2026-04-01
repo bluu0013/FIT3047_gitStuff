7636f14ea14912cce52f6c0f9bc8a5bfd0ec7a68
Balqis Kinanti Haldi Heryputri <bher0013@student.monash.edu>
2026-04-01 21:48:47 +1100

    Convert num strings to int

--- calc.py
+++ calc.py
@@ -1,9 +1,10 @@
 def compute(expression):
-    num0, operator, num1 = expression.split(' ')
-    if operator == '+':
-      return num0 + num1
-    elif operator == '-':
-      return num0 - num1
-    else:
-      print('unknown operator!')
-      return None
+  num0, operator, num1 = expression.split(' ')
+  num0, num1 = int(num0), int(num1)
+  if operator == '+':
+    return num0 + num1
+  elif operator == '-':
+    return num0 - num1
+  else:
+    print('unknown operator!')
+    return None
